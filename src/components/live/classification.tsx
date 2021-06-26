ebae69ba5e5f14c9d6dcc15b74a7dc8e828da121
mpapenbr <Markus.Papenbrock@gmail.com>
2021-06-26 14:53:39 +0200

    More compact standings Fixes #53

--- src/components/live/classification.tsx
+++ src/components/live/classification.tsx
@@ -1,16 +1,14 @@
-import { Card, Col, Empty, Row, Statistic, Table } from "antd";
-import { ColumnsType, TablePaginationConfig } from "antd/lib/table";
-import _ from "lodash";
+import { Card, Col, Empty, Row, Statistic } from "antd";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { sprintf } from "sprintf-js";
+import { useSelector } from "react-redux";
 import { ApplicationState } from "../../stores";
-import { classificationSettings } from "../../stores/ui/actions";
 import { getValueViaSpec } from "../../stores/wamp/compute/util";
 import { SessionManifest } from "../../stores/wamp/types";
-import { lapTimeString, secAsString } from "../../utils/output";
+import { secAsString } from "../../utils/output";
+import { Standings } from "../standings";
 
 const Classification: React.FC<{}> = () => {
+  const cars = useSelector((state: ApplicationState) => state.raceData.availableCars).map((v) => v.carNum);
   return (
     <>
       <Row>
@@ -26,118 +24,13 @@ const Classification: React.FC<{}> = () => {
 
       <Row>
         <Col span={24}>
-          <Standings />
+          <Standings showCars={cars} />
         </Col>
       </Row>
     </>
   );
 };
 export default Classification;
-
-const Standings: React.FC<{}> = () => {
-  const uiSettings = useSelector((state: ApplicationState) => state.userSettings.classification);
-  const carsRaw = useSelector((state: ApplicationState) => state.raceData.classification.data);
-  const stateCarManifest = useSelector((state: ApplicationState) => state.wamp.data.manifests.car);
-  const dispatch = useDispatch();
-  const getValue = (d: [], key: string) => getValueViaSpec(d, stateCarManifest, key);
-
-  const coloredTimeData = (d: [], key: string) => {
-    const value = getValueViaSpec(d, stateCarManifest, key);
-    if (typeof value === "number") {
-      return value > 0 ? lapTimeString(value) : "";
-    } else {
-      const [v, info] = value;
-      if (v < 0) return "";
-      if (info === "old") {
-        return <span style={{ color: "lightgrey" }}>{lapTimeString(v)}</span>;
-      }
-      if (info === "ob") {
-        return <span style={{ color: "purple", fontWeight: 500 }}>{lapTimeString(v)}</span>;
-      }
-      if (info === "pb") {
-        return <span style={{ color: "green", fontWeight: 500 }}>{lapTimeString(v)}</span>;
-      }
-      return v > 0 ? lapTimeString(v) : "";
-    }
-  };
-
-  const nullAwareOutput = (value: any, format: string): string => {
-    if (typeof value === "number") {
-      return sprintf(format, value);
-    } else return "";
-  };
-  const lapsOutput = (d: any) => {
-    if (getValue(d, "state") === "OUT") {
-      // console.log(getValue(d, "lc"));
-      return getValue(d, "lc");
-    } else return getValue(d, "lap");
-  };
-
-  const trackPosGraph = (d: any) => {
-    const pos = d * 100 + "%";
-    return (
-      <svg width="80px" height="12">
-        <rect x={0} y={0} width="100%" height="100%" style={{ stroke: "grey", fillOpacity: 0 }} />
-        <rect x={pos} y={0} width={2} height="100%" style={{ fill: "black" }} />
-      </svg>
-    );
-  };
-  const columns: ColumnsType<{}> = [
-    { key: "pos", title: "Pos", render: (d) => getValue(d, "pos") },
-    { key: "pic", title: "PIC", render: (d) => getValue(d, "pic") },
-    { key: "carNum", title: "Num", render: (d) => getValue(d, "carNum") },
-    { key: "carClass", title: "Class", render: (d) => getValue(d, "carClass") },
-    { key: "state", title: "State", render: (d) => getValue(d, "state") },
-    { key: "userName", title: "Driver", render: (d) => getValue(d, "userName") },
-    { key: "laps", title: "Lap", render: (d) => lapsOutput(d) },
-    { key: "last", title: "Last", render: (d) => coloredTimeData(d, "last") },
-    {
-      key: "best",
-      title: "Best",
-      render: (d) => {
-        const v = getValue(d, "best");
-        return v > 0 ? lapTimeString(v) : "";
-      },
-    },
-    // { key: "trackPos", title: "CurPos", render: (d) => nullAwareOutput(getValue(d, "trackPos"), "%.4f") },
-    { key: "trackPos2", title: "TrackPos", render: (d) => trackPosGraph(getValue(d, "trackPos")) },
-    { key: "dist", title: "Dist", render: (d) => nullAwareOutput(getValue(d, "dist"), "%.0f") },
-    { key: "gap", title: "Gap", render: (d) => nullAwareOutput(getValue(d, "gap"), "%.1f") },
-    { key: "interval", title: "Int", render: (d) => nullAwareOutput(getValue(d, "interval"), "%.1f") },
-    { key: "speed", title: "Speed", render: (d) => nullAwareOutput(getValue(d, "speed"), "%.0f") },
-  ];
-  stateCarManifest
-    .filter((v) => /^s\d+$/.test(v.name))
-    .forEach((v) =>
-      columns.push({
-        key: v.name,
-        title: v.name.toLocaleUpperCase(),
-        render: (d) => coloredTimeData(d, v.name),
-      })
-    );
-  // console.log(data);
-  // className="istint-compact"
-
-  const pagination: TablePaginationConfig = {
-    defaultPageSize: 20,
-    pageSize: uiSettings.pageSize,
-    onShowSizeChange: (curPage, newPageSize) => {
-      // console.log("current:" + curPage + " new: " + newPageSize);
-      // dispatch(uiClassificationSettings({ ...uiSettings, pageSize: newPageSize }));
-      dispatch(classificationSettings({ pageSize: newPageSize }));
-    },
-    showSizeChanger: true,
-  };
-  return (
-    <Table
-      className="iracelog-compact"
-      pagination={pagination}
-      columns={columns}
-      dataSource={carsRaw}
-      rowKey={() => _.uniqueId()}
-    />
-  );
-};
 
 const SessionInfoData: React.FC<{}> = () => {
   const sessionData: [] = useSelector(
