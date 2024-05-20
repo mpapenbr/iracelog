6908358ab9d696f8e3c2e2d9fec567fa0a75add4
mpapenbr <Markus.Papenbrock+github@gmail.com>
2024-05-20 09:10:15 +0200

    dynamic grpc config not working Fixes #1076

--- src/utils/useClient.ts
+++ src/utils/useClient.ts
@@ -12,23 +12,24 @@ import { globalWamp } from "../commons/globals";
 import { API_GRPC_BINARY_FORMAT } from "../constants";
 
 // This transport is going to be used throughout the app
-const transport = API_GRPC_BINARY_FORMAT
-  ? createGrpcWebTransport({
-      baseUrl: globalWamp.backendConfig.grpc.url,
-    })
-  : createConnectTransport({ baseUrl: globalWamp.backendConfig.grpc.url });
-
+const createTransport = () => {
+  return API_GRPC_BINARY_FORMAT
+    ? createGrpcWebTransport({
+        baseUrl: globalWamp.backendConfig.grpc.url,
+      })
+    : createConnectTransport({ baseUrl: globalWamp.backendConfig.grpc.url });
+};
 /**
  * Get a callback client for the given service.
  */
 export function useClient<T extends ServiceType>(service: T): CallbackClient<T> {
   // We memoize the client, so that we only create one instance per service.
-  return useMemo(() => createCallbackClient(service, transport), [service]);
+  return useMemo(() => createCallbackClient(service, createTransport()), [service]);
 }
 /**
  * Get a promise client for the given service.
  */
 export function usePromiseClient<T extends ServiceType>(service: T): PromiseClient<T> {
   // We memoize the client, so that we only create one instance per service.
-  return useMemo(() => createPromiseClient(service, transport), [service]);
+  return useMemo(() => createPromiseClient(service, createTransport()), [service]);
 }
