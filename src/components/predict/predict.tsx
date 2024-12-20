df00dbf910cefbe0fa41ad3e851e5e1f5ada6828
mpapenbr <Markus.Papenbrock+github@gmail.com>
2024-12-20 08:43:31 +0100

    small fix

--- src/components/predict/predict.tsx
+++ src/components/predict/predict.tsx
@@ -30,25 +30,38 @@ const PredictRace: React.FC<MyProps> = (props: MyProps) => {
   lData.parts
     .filter((item) => item.partType.case == "stint")
     .forEach((item) => {
-      const newEntry: IGraphData = {
+      const startEntry: IGraphData = {
         carNum: "leader",
         lapNo: "" + (item.partType.value as StintPart).lapStart,
         t: Number(item.start?.seconds) ?? 0,
       };
-      graphData.push(newEntry);
+      graphData.push(startEntry);
+      const endEntry: IGraphData = {
+        carNum: "leader",
+        lapNo: "" + (item.partType.value as StintPart).lapEnd,
+        t: Number(item.end?.seconds) ?? 0,
+      };
+      graphData.push(endEntry);
     });
   for (const [k, v] of Object.entries(predict.byCarNum)) {
     v.parts
       .filter((item) => item.partType.case == "stint")
       .forEach((item) => {
-        const newEntry: IGraphData = {
+        const startEntry: IGraphData = {
           carNum: k,
           lapNo: "" + (item.partType.value as StintPart).lapStart,
           t: Number(item.start?.seconds) ?? 0,
         };
-        graphData.push(newEntry);
+        graphData.push(startEntry);
+        const endEntry: IGraphData = {
+          carNum: k,
+          lapNo: "" + (item.partType.value as StintPart).lapEnd,
+          t: Number(item.end?.seconds) ?? 0,
+        };
+        graphData.push(endEntry);
       });
   }
+  graphData.sort((a, b) => parseInt(a.lapNo) - parseInt(b.lapNo));
   // .map((item) => ({lapNo: "12", t:12} as IGraphData))
   console.log(graphData);
   const config: LineConfig = {
@@ -57,6 +70,10 @@ const PredictRace: React.FC<MyProps> = (props: MyProps) => {
     xField: "lapNo",
     yField: "t",
     seriesField: "carNum",
+    point: {
+      size: 3,
+      shape: "diamond",
+    },
   };
   return graphData.length > 0 ? <Line {...config} /> : <Empty description="TBD" />;
 };
