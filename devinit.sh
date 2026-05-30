3cbdfc49d81cd43dcf23507262f50ccbef9c8936
mpapenbr <Markus.Papenbrock+github@gmail.com>
2026-05-30 10:25:40 +0200

    try to fix pnpm issues by going back to 11.3.0

--- devinit.sh
+++ devinit.sh
@@ -1,5 +1,8 @@
 # this script is supposed to be executed as postCreateCommand of the devcontainer
-corepack prepare pnpm@11.5.0 --activate
+# Note: back to 11.3.0 until issues with tarball integrity is fixed
+# see https://github.com/pnpm/pnpm/issues/12067 
+# produces way to many additional issues to handle right now
+corepack prepare pnpm@11.3.0 --activate
 pnpm config set @buf:registry  https://buf.build/gen/npm/v1/
 pnpm install
 if [ -f setuplinks.sh ]; then
