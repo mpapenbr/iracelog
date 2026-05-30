0551404ab2cb803e38711d6cf48c7c5883c2ad05
mpapenbr <Markus.Papenbrock+github@gmail.com>
2026-05-30 09:43:56 +0200

    fix pnpm setup + integrity for bufbuild

--- devinit.sh
+++ devinit.sh
@@ -1,4 +1,5 @@
 # this script is supposed to be executed as postCreateCommand of the devcontainer
+corepack prepare pnpm@11.5.0 --activate
 pnpm config set @buf:registry  https://buf.build/gen/npm/v1/
 pnpm install
 if [ -f setuplinks.sh ]; then
